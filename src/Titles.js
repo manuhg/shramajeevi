3dc0f7747663b8007c6b5c3c3adc2eafbabde585
manu-spherical <manuhegdev@gmail.com>
2018-11-12 00:17:17 +0530

    changeees

---
+++ src/Titles.js
@@ -0,0 +1,141 @@
+import AM1 from 'img/Thumbnails/english/AM1.jpeg';
+import AM2 from 'img/Thumbnails/english/AM2.jpeg';
+import BE from 'img/Thumbnails/english/BE.jpeg';
+import BK from 'img/Thumbnails/english/BK.jpeg';
+import CD1 from 'img/Thumbnails/english/CD1.jpeg';
+import CD2 from 'img/Thumbnails/english/CD2.jpeg';
+import CT from 'img/Thumbnails/english/CT.jpeg';
+import DLF from 'img/Thumbnails/english/DLF.jpeg';
+import EE from 'img/Thumbnails/english/EE.jpeg';
+import Goat from 'img/Thumbnails/english/Goat.jpeg';
+import Grape from 'img/Thumbnails/english/Grape.jpeg';
+import GT from 'img/Thumbnails/english/GT.jpeg';
+import HM1 from 'img/Thumbnails/english/HM1.jpeg';
+import HM2 from 'img/Thumbnails/english/HM2.jpeg';
+import ICB from 'img/Thumbnails/english/ICB.jpeg';
+import Mus from 'img/Thumbnails/english/Mus.jpeg';
+import OF1 from 'img/Thumbnails/english/OF1.jpeg';
+import OF2 from 'img/Thumbnails/english/OF2.jpeg';
+import PE from 'img/Thumbnails/english/PE.jpeg';
+import Pig from 'img/Thumbnails/english/Pig.jpeg';
+import Pom from 'img/Thumbnails/english/Pom.jpeg';
+import QD from 'img/Thumbnails/english/QD.jpeg';
+import Ra from 'img/Thumbnails/english/Ra.jpeg';
+import RWH from 'img/Thumbnails/english/RWH.jpeg';
+import SH from 'img/Thumbnails/english/SH.jpeg';
+import TG from 'img/Thumbnails/english/TG.jpeg';
+
+import B_K from 'img/Thumbnails/kannada/B-K.jpeg';
+import VH1_K from 'img/Thumbnails/kannada/VH1-K.jpeg';
+import VH2_K from 'img/Thumbnails/kannada/VH2-K.jpeg';
+import SK1_K from 'img/Thumbnails/kannada/SK1-K.jpeg';
+import SK2_K from 'img/Thumbnails/kannada/SK2-K.jpeg';
+import Ad_K from 'img/Thumbnails/kannada/Ad-K.jpeg';
+import Ku_K from 'img/Thumbnails/kannada/Ku-K.jpeg';
+import J_K from 'img/Thumbnails/kannada/J-K.jpeg';
+import K_K from 'img/Thumbnails/kannada/K-K.jpeg';
+import NT_K from 'img/Thumbnails/kannada/NT-K.jpeg';
+import QB_K from 'img/Thumbnails/kannada/QB-K.jpeg';
+import EK_K from 'img/Thumbnails/kannada/EK-K.jpeg';
+import MK_K from 'img/Thumbnails/kannada/MK-K.jpeg';
+import OSK_K from 'img/Thumbnails/kannada/OSK-K.jpeg';
+import KY1_K from 'img/Thumbnails/kannada/KY1-K.jpeg';
+import KY2_K from 'img/Thumbnails/kannada/KY2-K.jpeg';
+import MO_K from 'img/Thumbnails/kannada/MO-K.jpeg';
+import AN_K from 'img/Thumbnails/kannada/AN-K.jpeg';
+import Da_K from 'img/Thumbnails/kannada/Da-K.jpeg';
+import Dr_K from 'img/Thumbnails/kannada/Dr-K.jpeg';
+import BGT_K from 'img/Thumbnails/kannada/BGT-K.jpeg';
+import Ha_K from 'img/Thumbnails/kannada/Ha-K.jpeg';
+import Pa_K from 'img/Thumbnails/kannada/Pa-K.jpeg';
+import MPC1_K from 'img/Thumbnails/kannada/MPC1-K.jpeg';
+import MPC2_K from 'img/Thumbnails/kannada/MPC2-K.jpeg';
+import VA_K from 'img/Thumbnails/kannada/VA-K.jpeg';
+import TT_K from 'img/Thumbnails/kannada/TT-K.jpeg';
+
+import AK_H from 'img/Thumbnails/hindi/AK-H.jpeg';
+import Bakri_H from 'img/Thumbnails/hindi/Bakri-H.jpeg';
+import ICB_H from 'img/Thumbnails/hindi/ICB-H.jpeg';
+import JK1_H from 'img/Thumbnails/hindi/JK1-H.jpeg';
+import JK2_H from 'img/Thumbnails/hindi/JK2-H.jpeg';
+import Kalam_H from 'img/Thumbnails/hindi/Kalam-H.jpeg';
+import KY1_H from 'img/Thumbnails/hindi/KY1-H.jpeg';
+import KY2_H from 'img/Thumbnails/hindi/KY2-H.jpeg';
+import SBK_H from 'img/Thumbnails/hindi/SBK-H.jpeg';
+
+const titles = {
+  english: [
+    [AM1, 'Agricultural Machineries-1', ''],
+    [AM2, 'Agricultural Machineries-2', ''],
+    [BE, 'Banana Cultivation', ''],
+    [BK, 'Bee Keeping', ''],
+    [CD1, 'Commercial Dairying-1', ''],
+    [CD2, 'Commercial Dairying-2', ''],
+    [CT, 'Country Chicken and Turkey', ''],
+    [DLF, 'Dry Land Farming', ''],
+    [EE, 'Emu-An Introduction', ''],
+    [
+      Goat,
+      'Goat Farming',
+      'https://www.youtube.com/watch?v=ZCy7IMXFFcI&list=PLE1P7Fwrqlm9AA7AzJNaPTJsmKvZ8Lqi2&index=1',
+    ],
+    [Grape, 'Grape Cultivation', ''],
+    [GT, 'Grafting Techniques', ''],
+    [HM1, 'Herbal Veterinary Medicine-1', ''],
+    [HM2, 'Herbal Veterinary Medicine-2', ''],
+    [ICB, 'Indian Cow Breeds', ''],
+    [Mus, 'Mushroom Cultivation', ''],
+    [OF1, 'Organic Farming-1', ''],
+    [OF2, 'Organic Farming-2', ''],
+    [PE, 'Patchouli', ''],
+    [Pig, 'Pig Farming', ''],
+    [Pom, 'Pomegranate Cultivation', ''],
+    [QD, 'Japanese Quails and Ducks', ''],
+    [Ra, 'Rabbit Farming', ''],
+    [RWH, 'Rainwater Harvesting', ''],
+    [SH, 'Sheep Farming', ''],
+    [TG, 'Organic Terrace Garden', ''],
+  ],
+  kannada: [
+    [B_K, 'ವಾಣಿಜ್ಯ ಬಾಳೆ ಬೆಳೆ', ''],
+    [VH1_K, 'ವಾಣಿಜ್ಯ ಹೈನುಗಾರಿಕೆ – 1', ''],
+    [VH2_K, 'ವಾಣಿಜ್ಯ ಹೈನುಗಾರಿಕೆ – 2', ''],
+    [SK1_K, 'ಸಾವಯವ ಕೃಷಿ – 1', ''],
+    [SK2_K, 'ಸಾವಯವ ಕೃಷಿ – 2', ''],
+    [Ad_K, 'ಆಡು ಸಾಕಣೆ', ''],
+    [Ku_K, 'ಕುರಿ ಸಾಕಣೆ', ''],
+    [J_K, 'ಜೇನು ಕೃಷಿ', ''],
+    [K_K, 'ಕಸಿ ವಿಧಾನಗಳು', ''],
+    [NT_K, 'ನಾಟಿ ಕೋಳಿ ಮತ್ತು ಟರ್ಕಿ', ''],
+    [QB_K, 'ಜಪನೀಸ್ ಕ್ವೈಲ್ಸ್ ಮತ್ತು ಬಾತುಕೋಳಿ', ''],
+    [EK_K, 'ಎಮು – ಒಂದು ಪರಿಚಯ', ''],
+    [MK_K, 'ಮಳೆ ನೀರ ಕೊಯ್ಲು', ''],
+    [OSK_K, 'ಒಣ ಭೂಮಿಯಲ್ಲಿ ಸುಸ್ಥಿರ ಕೃಷಿ', ''],
+    [KY1_K, 'ಕೃಷಿ ಯಂತ್ರಗಳು – 1', ''],
+    [KY2_K, 'ಕೃಷಿ ಯಂತ್ರಗಳು – 2', ''],
+    [MO_K, 'ಮೊಲ ಸಾಕಣೆ', ''],
+    [AN_K, 'ಅಣಬೆ ಬೇಸಾಯ', ''],
+    [Da_K, 'ದಾಳಿಂಬೆ ಬೆಳೆ', ''],
+    [Dr_K, 'ದ್ರಾಕ್ಷಿ ಬೇಸಾಯ', ''],
+    [BGT_K, 'ಭಾರತೀಯ ಗೋ ತಳಿಗಳು', ''],
+    [Ha_K, 'ಹಂದಿ ಸಾಕಣೆ', ''],
+    [Pa_K, 'ಪಚೋಲಿ ಬೆಳೆ', ''],
+    [MPC1_K, 'ಗಿಡಮೂಲಿಕಾ ಪಶು ಚಿಕಿತ್ಸೆ – 1', ''],
+    [MPC2_K, 'ಗಿಡಮೂಲಿಕಾ ಪಶು ಚಿಕಿತ್ಸೆ – 2', ''],
+    [VA_K, 'ವೆನಿಲ್ಲಾ ಬೇಸಾಯ', ''],
+    [TT_K, 'ಸಾವಯವ ತಾರಸಿ ತೋಟ', ''],
+  ],
+  hindi: [
+    [AK_H, 'अंगूर की खेती', ''],
+    [Bakri_H, 'बकरी का पालन', ''],
+    [ICB_H, 'कृषि यंत्र – २', ''],
+    [JK1_H, 'जैविक खेती-१', ''],
+    [JK2_H, 'जैविक खेती – २', ''],
+    [Kalam_H, 'कलम बाँधने की तकनीक', ''],
+    [KY1_H, 'कृषि यंत्र – १', ''],
+    [KY2_H, 'कृषि यंत्र – २', ''],
+    [SBK_H, 'शुष्क भूमि की खेती', ''],
+  ],
+};
+
+export default titles;
--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -1,34 +1,64 @@
 import React, { Component } from 'react';
-
+import titles from 'Titles';
 class Videos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { langIndex: 0 };
+    this.languages = ['english', 'kannada', 'hindi'];
+  }
+  setLanguageIndex(langIndex) {
+    this.setState({ langIndex });
+    /*document.getElementById(this.languages[langIndex]).style.backgroundColor='#17a2b8';
+      document.getElementById(this.languages[langIndex]).style.color='white';*/
+  }
   render() {
+    const currentIndex = this.state.langIndex;
+    const currentLanguage = this.languages[currentIndex];
     return (
       <section id="videos" className="section_">
         <div className="album py-5 bg-light">
-          <div className="container">
+          <div className="container-fluid">
+            <div className="row text-center">
+              <div className="col-12 fp">
+                <div style={{ padding: '5px' }} className="btn-group" role="group" aria-label="Basic example">
+                  <button
+                    id="english"
+                    onClick={e => this.setLanguageIndex(0)}
+                    type="button"
+                    className={'btn btn-primary btn-' + (currentIndex !== 0 ? 'outline-' : '') + 'info'}
+                  >
+                    English
+                  </button>
+                  <button
+                    id="kannada"
+                    onClick={e => this.setLanguageIndex(1)}
+                    type="button"
+                    className={'btn btn-primary btn-' + (currentIndex !== 1 ? 'outline-' : '') + 'info'}
+                  >
+                    <h4>ಕನ್ನಡ</h4>
+                  </button>
+                  <button
+                    id="hindi"
+                    onClick={e => this.setLanguageIndex(2)}
+                    type="button"
+                    className={'btn btn-primary btn-' + (currentIndex !== 2 ? 'outline-' : '') + 'info'}
+                  >
+                    <h4>हिंदी</h4>
+                  </button>
+                </div>
+              </div>
+            </div>
             <div className="row">
-              <div className="col-md-4">
-                <div className="card mb-4 box-shadow">
-                  <img className="card-img-top" src="" alt="Card" />
-                  <div className="card-body">
-                    <p className="card-text">
-                      This is a wider card with supporting text below as a natural lead-in to additional content. This
-                      content is a little bit longer.
-                    </p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <button type="button" className="btn btn-sm btn-outline-secondary">
-                          View
-                        </button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary">
-                          Edit
-                        </button>
-                      </div>
-                      <small className="text-muted">9 mins</small>
+              {titles[currentLanguage].map((e, i) => (
+                <div key={i} className="py-2 col-4 col-md-3 col-lg-2 text-center">
+                  <div style={{ borderRadius: '0' }} className="card mb-4 box-shadow">
+                    <img className="card-img-top" src={e[0]} alt={e[1]} />
+                    <div className="card-body">
+                      <p className="card-text text-center">{e[1]}</p>
                     </div>
                   </div>
                 </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
