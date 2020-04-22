a60675a5a3e030dc5f5a60954c38441a597bb98f
spherical-gk1000 <manuhegdev@gmail.com>
2020-04-22 21:06:19 +0530

    updated home content

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,57 +16,72 @@ class About extends Component {
           <div className="row">
             <div className="col-10 offset-1 text-justify">
               <p>
-                SHRAMAJEEVI, basically, is a Visual Media Production House dedicated to agriculture. Produces
-                educative-technical agricultural documentaries, corporate films and ads. It has websites providing
-                services to farming community. It was established in the year 2004 as Shramajeevi Agri Films. It has
-                produced around 75 agricultural documentary films in Kannada, English and Hindi, which are technically
-                accurate and practically applicable. Shramajeevi videos are being used widely as training material by
-                KVKs, DATCs, Training Centres, NGOs and other organisations. Till 2016 those documentaries were sold in
-                the form of DVDs across India, Sri Lanka and Bangladesh. We have shared these agricultural videos with
-                many TV channels across India and other countries. Media organisations from Kenya, Afghanistan,
-                Hong-Kong, South Korea, Nepal, USA and Sri Lanka have telecast our documentaries. Now, from the year
-                2013, Shramajeevi videos are available online on 3 YouTube Channels – Shramajeevi Agri Films, AgVid and
-                AgCorp.
+                SHRAMAJEEVI is a visual media production house dedicated to agriculture. Produces educative, technical
+                and practically applicable agricultural video documentaries. It was established in the year 2004 as
+                Shramajeevi Agri Films (SAF), a proprietary firm. It produced hundreds of agricultural documentaries,
+                corporate films, ads, jingles etc. in many languages. Shramajeevi has worked for either agriculture or
+                general AV projects of Agricultural Universities, Research Organizations, Government Departments and
+                Boards, NGOs, Corporate companies and others. Shramajeevi has an in-house team of agricultural media
+                professionals for script writing, translation, voice over and other post-production works of visual
+                media. Shramajeevi has all necessary technical infrastructure and a huge visual database to produce any
+                kind of audio and video film
               </p>
               <p>
-                We are preparing ourselves to launch a satellite TV channel soon exclusive for agriculture in Kannada, a
-                regional language of India under the company Shramajeevi Television Private Limited, Bengaluru.
-                Shramajeevi has all necessary technical infrastructure and a huge visual database to produce any kind of
-                audio and video films. We have worked on AV projects for Agricultural Universities, Government
-                Departments and Boards, NGOs, Companies and others. Shramajeevi has an in-house team of agricultural
-                media professionals for writing scripts, translation, voice over and other production works of visual
-                media. We do work for general Audio-Visual projects as well.{' '}
-              </p>
-              <p>
-                {' '}
-                In the year 2016, Shramajeevi has launched an exclusive image website{' '}
-                <a href="https://shramajeeviimages.com" target="_blank" rel="noopener noreferrer">
-                  <u>shramajeeviimages.com</u>
+                Shramajeevi has shared its contents with many media houses and TV channels in India and abroad.
+                Organizations from Kenya, Afghanistan, Hong-Kong, South Korea, Japan, Nepal, USA, Sri Lanka, Zambia etc.
+                have used Shramajeevi content for their telecast or training. From 2012 Shramajeevi videos are available
+                on its 3 popular YouTube channels –{' '}
+                <a href="https://www.youtube.com/user/ShramajeeviAgriFilms" target="_blank" rel="noopener noreferrer">
+                  <u>Shramajeevi TV</u>
+                </a>
+                ,{' '}
+                <a href="https://www.youtube.com/c/AgVidShramajeevi" target="_blank" rel="noopener noreferrer">
+                  <u>AgVid</u>
                 </a>{' '}
-                for free download of agricultural and rural images and{' '}
-                <a href="https://shramajeevicontacts.com" target="_blank" rel="noopener noreferrer">
-                  <u>shramajeevicontacts.com</u>
-                </a>{' '}
-                a digital directory for agriculture. In the year 2017, we have brought out another website{' '}
-                <a href="shramajeewiki.com" target="_blank" rel="noopener noreferrer">
-                  <u>shramajeewiki.com</u>
-                </a>{' '}
-                for knowledgeable articles on agriculture. In June 2018 we have launched the business version of
-                agriculture digital directory as{' '}
+                and{' '}
+                <a
+                  href="https://www.youtube.com/channel/UCA1FVotTUeuqFoA1kudWyLQ"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <u>AgCorp</u>
+                </a>
+                . Shramajeevi has 3 agriculture service websites namely, digital directory for agriculture –{' '}
                 <a href="https://agdial.in" target="_blank" rel="noopener noreferrer">
                   <u>agdial.in</u>
                 </a>
-                . In near future Shramajeevi is getting in to E-commerce business exclusive for agriculture with{' '}
-                <a href=" ">
-                  <u>agtrade.in</u>
+                , for agricultural and rural images –{' '}
+                <a href="https://shramajeeviimages.com" target="_blank" rel="noopener noreferrer">
+                  <u>shramajeeviimages.com</u>
+                </a>{' '}
+                and for knowledgeable agricultural articles –{' '}
+                <a href="shramajeewiki.com" target="_blank" rel="noopener noreferrer">
+                  <u>shramajeewiki.com</u>
                 </a>
                 .{' '}
               </p>
               <p>
-                Shramajeevi is lead by its Founder and CMD DR. Venkatramana Hegde. He is basically a farmer, an
-                agriculture graduate, writer and visual media professional. Earlier he worked for ETV Kannada Annadata
-                as program head at RFC, Hyderabad. Later with Kasturi TV Bengaluru as nonfiction producer. Now DR.
-                Hegde, as CMD, looks after all agricultural technical issues at Shramajeevi for all its projects.
+                {' '}
+                In the year 2016, Shramajeevi Television Private Limited, a private company was registered. The main
+                purpose is to launch 24x7 satellite TV channel dedicated to agriculture in Kannada, to begin with and
+                also to establish IT & mass-media based agriculture knowledge & communication ecosystem. STPL works in
+                coordination with many agricultural universities and agriculture research institutes of India, various
+                departments of the central and state governments, agriculture corporate companies, farmers and other
+                stakeholders of agriculture for content development.
+              </p>
+              <p>
+                Shramajeevi is led by its Founder and CMD DR. Venkatramana Hegde. He is basically a farmer, an
+                agriculture graduate, writer and visual media professional. Earlier he served as head of Annadata
+                program of ETV Kannada at Ramoji Film City, Hyderabad and as nonfiction producer at Kasturi TV
+                Bengaluru. Now, DR. Hegde, as CMD, looks after all agricultural technical issues at Shramajeevi for all
+                its projects.
+              </p>
+              <p>
+                <b>Vision:</b> To become one stop solution for all needs of farmers and other stakeholders of farming
+                and to harvest IT & Mass media tools to their potential to benefit farming sector.{' '}
+              </p>
+              <p>
+                <b>Mission:</b> To make farming competitive, sustainable, profitable and a respected profession.
               </p>
             </div>
           </div>
