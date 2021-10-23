886cddc957da2c80fb0446aad3f891fe26b4c61a
Manu Hegde <manuhegdev@gmail.com>
2021-10-23 22:50:17 +0530

    add missing files

---
+++ src/MainPage.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import 'css/App.css';
+import 'css/styles.css';
+
+import Nav from 'components/Nav';
+import Footer from 'components/Footer';
+import Sidebar from 'components/Sidebar';
+
+import Home from 'pages/Home';
+import About from 'pages/About';
+import Contact from 'pages/Contact';
+
+class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 0, height: 0 };
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Home />
+        <Sidebar />
+        <About />
+        {/*<Clients />*/}
+        {/*<Videos />*/}
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
+
+  updateDimensions() {
+    var w = window,
+      d = document,
+      documentElement = d.documentElement,
+      body = d.getElementsByTagName('body')[0],
+      width = w.innerWidth || documentElement.clientWidth || body.clientWidth,
+      height = w.innerHeight || documentElement.clientHeight || body.clientHeight;
+
+    this.setState({ width, height });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions.bind(this));
+  }
+}
+
+export default MainPage;
